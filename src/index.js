c260d4f8e56090a904812254ecdde88786793eea
Rennie Bevineau <rsbevineau@gmail.com>
2021-03-06 19:53:56 -0500

    connect react with redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import store from "./js/store/index"
+import { Provider } from "react-redux"
+import App from "./js/components/App";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
+
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
